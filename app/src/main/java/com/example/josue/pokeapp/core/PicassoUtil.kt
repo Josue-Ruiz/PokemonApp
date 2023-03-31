fd1fcf3ebfc687440483c6d019e1d892e1e79344
Josue Ruiz <josueruiz_sz@outlook.com>
2023-03-31 00:40:22 -0600

    load the image with picasso

---
+++ app/src/main/java/com/example/josue/pokeapp/core/PicassoUtil.kt
@@ -0,0 +1,11 @@
+package com.example.josue.pokeapp.core
+
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+import javax.inject.Inject
+
+class PicassoUtil @Inject constructor(private val picasso: Picasso){
+    fun loadImage(url: String,imageview: ImageView){
+        picasso.load(url).into(imageview)
+    }
+}
--- app/src/main/java/com/example/josue/pokeapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/josue/pokeapp/ui/view/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.josue.pokeapp.ui.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
+import com.example.josue.pokeapp.core.PicassoUtil
 import com.example.josue.pokeapp.databinding.ActivityMainBinding
 import com.example.josue.pokeapp.ui.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var viewModel: MainViewModel
+
+    @Inject
+    lateinit var picassoloader: PicassoUtil
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -25,8 +29,9 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.onCreate()
 
-        viewModel.pokemon.observe(this, Observer{
-            binding.pkemon.text = it.urlImage
-        })
+        viewModel.pokemon.observe(this) {
+            binding.pkemon.text = "#${it.id} ${it.name} ${it.weight}"
+            picassoloader.loadImage(it.urlImage, binding.ivPokemon)
+        }
     }
 }
