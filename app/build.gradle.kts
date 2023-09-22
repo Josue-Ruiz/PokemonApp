afcdff21ab86cc5dfeefa7f2a3900dab327c1400
Josue Ruiz <josueruiz_sz@outlook.com>
2023-09-21 23:44:00 -0600

    Upgrade(Gradle) upgrade compileSdk and targetSdk in gradle app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.josue.pokeapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.josue.pokeapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
