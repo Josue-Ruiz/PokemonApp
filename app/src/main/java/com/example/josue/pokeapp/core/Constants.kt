672f410c38bbc3ca4d54c0e8b6846b013dd59d36
Josue Ruiz <josueruiz_sz@outlook.com>
2023-10-15 15:54:47 -0600

    Chore(Detekt) solve problems in model package MagicNumber GetPokemonUseCase file add Constants file for MagicNumber detekt problem

---
+++ app/src/main/java/com/example/josue/pokeapp/core/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.josue.pokeapp.core
+
+object Constants {
+    const val LAST_POKEMON_NUMBER = 1010
+}
--- app/src/main/java/com/example/josue/pokeapp/domain/GetPokemonUseCase.kt
+++ app/src/main/java/com/example/josue/pokeapp/domain/GetPokemonUseCase.kt
@@ -1,12 +1,13 @@
 package com.example.josue.pokeapp.domain
 
+import com.example.josue.pokeapp.core.Constants.LAST_POKEMON_NUMBER
 import com.example.josue.pokeapp.data.PokemonRepository
 import com.example.josue.pokeapp.domain.model.Pokemondata
 import javax.inject.Inject
 
 class GetPokemonUseCase @Inject constructor(private val repository: PokemonRepository) {
     suspend operator fun invoke(): Pokemondata {
-        val id = (1..1010).random()
+        val id = (1..LAST_POKEMON_NUMBER).random()
         return repository.getPokemon(id)
     }
 }
