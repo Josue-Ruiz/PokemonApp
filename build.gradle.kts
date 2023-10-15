a9e65efdba1822781e9b4b5bb95cd3748cf82659
Josue Ruiz <josueruiz_sz@outlook.com>
2023-10-15 14:05:54 -0600

    Chore(Gradle) add detekt to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.android.library") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.1" apply false
 }
 
 buildscript{
